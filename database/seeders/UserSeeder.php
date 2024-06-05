99dc762ed58ceac3b0b0f5361926ab58cd77179d
sistemas04 <sistemas04@woobsing.com>
2024-06-05 11:47:11 -0400

    Create fatories and seeder to users and leads

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,17 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\Lead;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class LeadSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Lead::factory()->count(10)->create();
+        User::factory()->count(10)->create();
     }
 }
