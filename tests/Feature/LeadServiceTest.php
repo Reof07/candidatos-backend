a78d178ffd7594d402f00c3cd82e79f9b7476bf7
sistemas04 <sistemas04@woobsing.com>
2024-06-06 21:19:15 -0400

    test

--- tests/Feature/LeadServiceTest.php
+++ tests/Feature/LeadServiceTest.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use App\Repositories\LeadRepository;
 use App\Services\LeadService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
